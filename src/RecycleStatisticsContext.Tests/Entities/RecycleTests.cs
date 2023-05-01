2f4030067c8ee168b8ab4fd1d64a395abf4a05b0
Romario de Melo Damasceno <romario.damasceno@freto.com>
2023-05-01 18:00:09 -0300

    Create Recycle

--- src/RecycleStatisticsContext.Tests/Entities/RecycleTests.cs
+++ src/RecycleStatisticsContext.Tests/Entities/RecycleTests.cs
@@ -1,32 +1,71 @@
-﻿using Xunit;
+﻿using ExpectedObjects;
+using System.Security.Cryptography;
+using Xunit;
 
 namespace RecycleStatisticsContext.Tests.Entities
 {
     public class RecycleTests
     {
+        private readonly Recycle _recycle;
+
+        public RecycleTests()
+        {
+            var typeRecycle = new TypeRecycle("Oil kitchen", Guid.NewGuid());
+            _recycle = new Recycle(typeRecycle.Id, 1);
+        }
+
         [Fact]
         public void Empty()
         {
         }
 
         [Fact]
-        public void CreateRecycle()
+        public void CreateFirstRecycle()
         {
-            var recycle = new Recycle("Oil kitchen", 1);
+            Assert.Equal(_recycle.TypeRecycle, _recycle.TypeRecycle);
+            Assert.Equal(1, _recycle.Count);
+        }
 
-            Assert.Equal("Oil kitchen", recycle.TypeRecycle);
-            Assert.Equal(1, recycle.Count);
+        [Fact]
+        public void CreateRecycleWithType_Plastic()
+        {
+            TypeRecycle typeRecycle = new TypeRecycle("Plastic", Guid.NewGuid());
+
+            var recycle = new
+            {
+                TypeRecycleId = typeRecycle.Id,
+                Count = 1
+            }.ToExpectedObject();
+
+            var recycleActual = new Recycle(typeRecycle.Id, 1);
+
+            recycle.ShouldMatch(recycleActual);
+        }
+    }
+
+    public class TypeRecycle
+    {
+        public string? Type { get; set; }
+        public Guid Id { get; set; }
+
+        public TypeRecycle() {}
+
+        public TypeRecycle(string? type, Guid id)
+        {
+            Type = type;
+            Id = id;
         }
     }
 
     public class Recycle
     {
-        public string TypeRecycle { get; set; }
-        public int Count { get; set; }
+        public TypeRecycle TypeRecycle { get; private set; }
+        public Guid TypeRecycleId { get; private set; }
+        public int Count { get; private set; }
 
-        public Recycle(string typeRecycle, int count)
+        public Recycle(Guid typeRecycleId, int count)
         {
-            TypeRecycle = typeRecycle;
+            TypeRecycleId = typeRecycleId;
             Count = count;
         }
     }
