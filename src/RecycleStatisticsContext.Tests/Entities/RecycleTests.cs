2f69cd030f1d626f2f0a9267ed6d600adb612a6d
Romario de Melo Damasceno <romario.damasceno@freto.com>
2023-04-30 22:20:07 -0300

    First tests with Recycle Entity

---
+++ src/RecycleStatisticsContext.Tests/Entities/RecycleTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace RecycleStatisticsContext.Tests.Entities
+{
+    public class RecycleTests
+    {
+        [Fact]
+        public void Empty()
+        {
+        }
+
+        [Fact]
+        public void CreateRecycle()
+        {
+            var recycle = new Recycle("Oil kitchen", 1);
+
+            Assert.Equal("Oil kitchen", recycle.TypeRecycle);
+            Assert.Equal(1, recycle.Count);
+        }
+    }
+
+    public class Recycle
+    {
+        public string TypeRecycle { get; set; }
+        public int Count { get; set; }
+
+        public Recycle(string typeRecycle, int count)
+        {
+            TypeRecycle = typeRecycle;
+            Count = count;
+        }
+    }
+}
+
